631ddcc95a396db1520f57c362b74663da28748d
Curtis Dunne <curtdunne@gmail.com>
2019-04-02 15:23:59 -0600

    Revert "Added Mailgun settings to app and removed shitty assed SendGrid"
    This reverts commit 159fc3963d7e8cc43adf0717d802b588fb357dab.

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN _SMTP_PORT'],
-  :addresses      => ENV['MAILGUN _SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN _SMTP_LOGIN'],
-  :password       => ENV['MAILGUN _SMTP_PASSWORD'],
-  :domain         => 'photo-app-dunne.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials[:sendgrid][:user_name],
+    :password => Rails.application.credentials[:sendgrid][:password],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
